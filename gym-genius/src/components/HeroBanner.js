749dbad2c03199c7541258a490e6c6d2b6dcfb17
ebheto <ebhetome@protonmail.com>
2023-03-23 03:40:09 +0000

    search exercise bar

--- gym-genius/src/components/HeroBanner.js
+++ gym-genius/src/components/HeroBanner.js
@@ -1,27 +1,40 @@
 import React from 'react'
 import {Box, Stack, Typography, Button} from '@mui/material';
 
+import HeroBannerImage from '../assets/images/Banner.png';
+import { fontSize } from '@mui/system';
+
 const HeroBanner = () => {
   return (
     <Box sx={{
-        mt: { lg :'212px', xs: '70px'},
-        ml: { sm: '50px'}
+      mt: { lg: '212px', xs: '70px' },
+      ml: { sm: '50px' }
     }} position="relative" p="20px">
       <Typography color="#FF2625"
-      fontWeight="550" fontSize="25px">
-        Fitness Club 
-      </Typography>  
-      <Typography fontWeight={700}
-      sx={{ fontSize: { lg: '44px', sm: '40px'} }}>
-        No e=mc2 <br /> with these workouts 
+        fontWeight="550" fontSize="25px">
+        Fitness Club
       </Typography>
-      <Typography fontSize="22px"
-      lineHeight={3.5}>
-        Check out these exercises that leave you feeling like a genius
+      <Typography fontSize="60px" fontWeight={500}
+        lineHeight={1.5} mb={2}>
+        it's mind over matter
       </Typography>
-      <Button>
-
-      </Button>
+      <Button variant="contained"
+        color='error' href='#exercises'
+        sx={{ backgroundColor: '#FF2625', 
+        padding: '10px' }}>
+      Explore Exercises
+    </Button>
+    <Typography fontWeight={600}
+      color="#00bfff"
+      sx={{
+        opacity: 0.1,
+        display: { lg: 'block', xs: 'none' },
+        fontSize: "200px",
+        mt: { lg: '150px' }
+      }}>
+        Exercise
+      </Typography><img src={HeroBannerImage} alt="banner"
+        className='hero-banner-img' />
     </Box>
   )
 }
--- gym-genius/src/components/SearchExercises.js
+++ gym-genius/src/components/SearchExercises.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import { Box, Stack, Button, TextField, Typography} from '@mui/material'; 
+import { fontWeight } from '@mui/system';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px"
+    justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{
+    fontSize: { lg: '44px', xs: '30px'}}}
+    mb="50px" ml="50px" textAlign="centre" >
+      Check out these Workouts
+      </Typography>
+      <Box position="center" mb="72px">
+        <TextField 
+        sx={{ 
+          input: { 
+          fontWeight: '700' },
+          border: 'none',
+          borderRadius: '4px',
+          width: {
+            lg: '1170px', xs: '350px'
+          }
+        }}
+        height="76px"
+        value=""
+        onChange={(e) => {}}
+        placeholder="Search Exercises"
+        type="text"       
+        />
+      </Box>
+    </Stack>
   )
 }
 
 export default SearchExercises
+
